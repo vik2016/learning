1cd2273fd39700d40c279dcce061adbdb91c1cf6
ESHAARVEER <vikramjsbajwa@gmail.com>
2016-06-16 14:50:24 +0530

    email validation

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-var test;
+$(".subscribeForm .primary").click(function (evt) {
+     evt.preventDefault();
+    var email = $("#subscribe").val();
+
+    if (isEmailValid(email)) {
+        $('.error').hide();
+        console.log('valid email');
+    }
+    else if (!isEmailValid(email)) {
+        $('.error').show();
+        console.log('invalid email');
+    }
+});
+function isEmailValid(emailAdress) {
+    var rexp = new RegExp('^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$', 'i');
+//  console.log(rexp.test(emailAdress))
+    return rexp.test(emailAdress)
+}
