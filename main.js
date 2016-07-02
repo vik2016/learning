f0c5e4842aa1c4ced8aff2c678b9e70c85b8002e
ESHAARVEER <vikramjsbajwa@gmail.com>
2016-07-02 17:54:33 +0530

    added data-id with each function and removed someclass and if

--- main.js
+++ main.js
@@ -23,33 +23,35 @@ var myCode = {
         $(document).ready(function () {
             var data = 'http://jsonplaceholder.typicode.com/users';
             $.getJSON(data, function (data) {
-                console.log(data);
+
                 for (var i = 0; i < data.length; i++) {
-                    console.log(i)
-                    var name = data[i].name.slice(0,8);
-                    $(".outer").append('<div class="names" data-id = ' + data[i].id + '>' + name + '<ul class="submenu"></ul></div>');
+                    var name = data[i].name.slice(0, 8);
+                    $(".outer").append('<div class="names">' + name + '<ul class="submenu"></ul></div>');
                 }
-                 $('.names').mouseenter(function (e) {
+                $('.names').mouseenter(function (e) {
+
                     $('ul').empty();
                     e.preventDefault();
                     e.stopPropagation();
-                    $('.names').addClass('someClass');
-                    if($(this).hasClass('someClass')) {
-                            $('ul').show();
-                       var returnedId = $(this).attr("data-id")-1;
-                      //  console.log(returnedId);
+                    // $('.names').addClass('someClass');
+                    $('.names').each(function (j) {
+                        $(this).attr('data-id', +(j))
+
+                    })
+
+                   var returnedId = $(this).attr("data-id");
                         $(this).children('ul').append('<li class="test">' +
-                            '<div>'+data[returnedId].email+'</div>' +
-                            '<div>'+data[returnedId].username+'</div>'+
-                            '<div>'+data[returnedId].website+'</div>'+
-                            '<div>'+data[returnedId].phone+'</div>' +
-                            '<div>'+data[returnedId].address.street+'</div>'+
-                            '<div>'+data[returnedId].address.city+'</div>'+
+                            '<div>' + data[returnedId].email + '</div>' +
+                            '<div>' + data[returnedId].username + '</div>' +
+                            '<div>' + data[returnedId].website + '</div>' +
+                            '<div>' + data[returnedId].phone + '</div>' +
+                            '<div>' + data[returnedId].address.street + '</div>' +
+                            '<div>' + data[returnedId].address.city + '</div>' +
                             '</li>');
-                    }
+
                 }).mouseleave(function () {
-                     $('ul').empty();
-                 })
+                    $('ul').empty();
+                })
             })
         });
     }
